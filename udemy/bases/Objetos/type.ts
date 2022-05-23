848497bfcbb123cb3d232a53c5b8ac371e1889dd
Oscar Rdgz <raadgtooz@gmail.com>
2022-05-22 20:20:04 -0500

    objects

---
+++ udemy/bases/Objetos/type.ts
@@ -0,0 +1,19 @@
+(()=>{
+
+    let flash: { name:string, age: number, powers: string[], getName?:(x:void)=>string } = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    } //Se establece el tipo de flash automaticamente, por lo que solo recibe las propiedades que se especificaron aqui
+
+    let superman: { name:string, age: number, powers: string[], getName?:(x:void)=>string } = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super velocidad']
+    }
+    
+
+
+
+
+})()
