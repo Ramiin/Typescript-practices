0c3d5c77612dca162a3d03224e5b1485cd51d23b
Oscar Rdgz <raadgtooz@gmail.com>
2022-05-22 20:31:27 -0500

    type

--- udemy/bases/Objetos/type.ts
+++ udemy/bases/Objetos/type.ts
@@ -1,15 +1,25 @@
 (()=>{
 
-    let flash: { name:string, age: number, powers: string[], getName?:(x:void)=>string } = {
+    type Hero = {
+        name: string;
+        age?: number;
+        powers: string[];
+        getName?: () => string
+    }
+
+    let flash: Hero = {
         name: 'Barry Allen',
         age: 24,
         powers: ['Super velocidad', 'Viajar en el tiempo']
     } //Se establece el tipo de flash automaticamente, por lo que solo recibe las propiedades que se especificaron aqui
 
-    let superman: { name:string, age: number, powers: string[], getName?:(x:void)=>string } = {
+    let superman: Hero = {
         name: 'Clark Kent',
         age: 60,
-        powers: ['Super velocidad']
+        powers: ['Super velocidad'],
+        getName(){
+            return this.name;
+        }
     }
     
 
