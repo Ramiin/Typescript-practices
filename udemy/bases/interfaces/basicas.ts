8785c566fc29ef6df3f124bea2a36c1b35437bec
Oscar Rdgz <raadgtooz@gmail.com>
2022-06-14 18:07:48 -0500

    basic interfaces

---
+++ udemy/bases/interfaces/basicas.ts
@@ -0,0 +1,41 @@
+(()=>{
+
+
+    //Diferencia entre tipos e interfaces:
+    //Tipo: no puede extenderse, es fijo
+    //Interface se pueden extender o expandir, agregar propiedades, etc
+
+    //Escrito en Type:
+    // type Hero = {
+    //     name: string;
+    //     age?: number;
+    //     powers: string[];
+    //     getName?: () => string
+    // }
+
+    //Escrito en interface
+    interface Hero  {
+        name: string;
+        age?: number;
+        powers: string[];
+        getName?: () => string
+    }
+
+    let flash: Hero = {
+        name: 'Barry Allen',
+        age: 24,
+        powers: ['Super velocidad', 'Viajar en el tiempo']
+    } //Se establece el tipo de flash automaticamente, por lo que solo recibe las propiedades que se especificaron aqui
+
+    let superman: Hero = {
+        name: 'Clark Kent',
+        age: 60,
+        powers: ['Super velocidad'],
+        getName(){
+            return this.name;
+        }
+    }
+
+
+
+})()
