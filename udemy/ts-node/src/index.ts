c19ef92b8e0098ce45e878e322bd52b0fc254e69
Oscar Rdgz <raadgtooz@gmail.com>
2022-07-01 08:27:37 -0500

    Exports and imports

--- udemy/ts-node/src/index.ts
+++ udemy/ts-node/src/index.ts
@@ -1,9 +1,13 @@
+// import { Hero as SuperHero } from './classes/Hero';
+import * as HeroClasses from './classes/Hero'
+import powers, { Power } from './data/powers'
 
+// const ironman = new SuperHero('Tony Stark', 1, 55)
+const ironman = new HeroClasses.Hero('Tony Stark', 1, 55)
 
-console.log('Hola Mundo!');
+console.log(ironman)
 
-
-
+console.log(powers)
 
 
 
